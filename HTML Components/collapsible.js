3e9856e1ccfb069eedbfe447b5139b650de349e1
Nadroj1234 <jordank06759@gmail.com>
2025-04-24 10:22:55 -0400

    components

---
+++ HTML Components/collapsible.js
@@ -0,0 +1,9 @@
+//get all elments with class of collapsible//
+const collapsibles = document.querySelectorAll('.collapsible')
+console.log(collapsibles);
+// loop through each element we selected//
+collapsibles.forEach((item) => { // we'll call each element "item"//
+    item.addEventListener("click", function(){
+        this.classList.toggle("collapsible-expanded")
+    })
+})
